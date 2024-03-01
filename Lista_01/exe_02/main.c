7ae3880e8a021eade2565c054b27e28ce9df12f5
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-02-29 21:46:31 -0300

    Update

---
+++ Lista_01/exe_02/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//02) Altere o código do exercício 01 para exibir um nome (ou sobrenome) em cada linha.
+
+int main () {
+
+    printf("Diogo \n");
+    printf("Marinho\n");
+    return 0;
+
+}
---
+++ Lista_01/exe_03/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*03) Faça um código que receba um número inteiro do usuário e exiba na tela:
+O número digitado
+O seu dobro
+O seu triplo
+*/
+
+int main () {
+
+}
