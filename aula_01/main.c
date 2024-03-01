9f7bc3202a1e41eea81cae187f448173fe5be968
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-02-29 21:16:46 -0300

    Update

--- aula_01/main.c
+++ aula_01/main.c
@@ -7,5 +7,6 @@ int main() {
     printf("%d\n", myNum);
     printf("%f\n", myFloat);
     printf("%c\n", myLetter);
+    printf("Meu número é %d e meu float é %f e meu char é %c\n, myNum, myFloat, myLetter");
     return 0;
 }
