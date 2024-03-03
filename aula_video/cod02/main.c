cd733b4e944db3db8f87aa86b30ea345f8ceba6c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-03 13:18:14 -0300

    Update

---
+++ aula_video/cod02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+
+   /* escape sequence = character combinatin consistig of a backslash
+                        followed by a letter or combination of digits.
+                        They specify actions within a line or string of Text.
+                        \n = newline 
+                        \t = tab
+    */
+
+    printf("I like pizza!\n");
+    printf("1\n 2\n 3\n");
+    return 0;
+}
---
+++ aula_video/cod04/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    char a = 'C';                   // single character  %c
+    char b[] ="Bro";                // array of characters %s
+
+    float c = 3.141592;             // 4 bytes (32 bits of precision) 6 - 7 digits %f
+    // double d = 3.141592653589793; // 8 bytes (64 bits of precision) 15 - 16 digits %1f
+
+    // bool e = true;               // 1 byte (true or false) %d
+
+    //char f = 100;                 // 1 byte (-128 to +127) %d or %c
+    //unsigned char g = 255;        // 1 byte (0 to +255) %d or %c
+
+    //short int h = 32767;          //2 bytes (-32,768 to +32,767) %d
+    //usigned int k  = 
+}
