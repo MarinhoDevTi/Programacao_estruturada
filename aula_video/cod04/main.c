2c230021be82f5f18f1d3302adf4d6ea87e83c76
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-03 14:46:26 -0300

    Update main.c

--- aula_video/cod04/main.c
+++ aula_video/cod04/main.c
@@ -7,7 +7,7 @@ int main(){
     char b[] ="Bro";                // array of characters %s
 
     float c = 3.141592;             // 4 bytes (32 bits of precision) 6 - 7 digits %f
-    // double d = 3.141592653589793; // 8 bytes (64 bits of precision) 15 - 16 digits %1f
+    double d = 3.141592653589793; // 8 bytes (64 bits of precision) 15 - 16 digits %1f
 
     // bool e = true;               // 1 byte (true or false) %d
 
@@ -15,5 +15,10 @@ int main(){
     //unsigned char g = 255;        // 1 byte (0 to +255) %d or %c
 
     //short int h = 32767;          //2 bytes (-32,768 to +32,767) %d
-    //usigned int k  = 
+    //unsigned short int i = 65535; // 2 bytes (0 to +65,535) %d
+
+    int j = 2147483647;             // 4 bytes ( -2,147,483,648 to +2,147,483,647) %d
+    //unsigned int k = 4294967295L; // 4 bytes ( 0 to +4,294, 967, 295) %u
+
+    //long long int 1 =  9223370368547775807;       // 8 bytes (-9 )
 }
