4243f3973b93f78da2e558864867b549a48b1494
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-04 22:01:48 -0300

    Update

--- Desafio_01/main.c
+++ Desafio_01/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main () {
-    
+int main() {
+    double distancia_km, velocidade_luz_ms;
+    double tempo_segundos, tempo_minutos, tempo_horas;
+
+    // Solicitar ao usuário que insira a distância entre dois planetas em quilômetros
+    printf("Digite a distância entre dois planetas em quilômetros: ");
+    scanf("%lf", &distancia_km);
+
+    // Definir a velocidade da luz em metros por segundo
+    velocidade_luz_ms = 299792458.0;
+
+    // Calcular o tempo que levaria para viajar entre os dois planetas na velocidade da luz
+    tempo_segundos = (distancia_km * 1000) / velocidade_luz_ms;
+    tempo_minutos = tempo_segundos / 60;
+    tempo_horas = tempo_minutos / 60;
+
+    // Imprimir o tempo em segundos, minutos e horas
+    printf("Tempo para viajar na velocidade da luz:\n");
+    printf("Segundos: %lf\n", tempo_segundos);
+    printf("Minutos: %lf\n", tempo_minutos);
+    printf("Horas: %lf\n", tempo_horas);
+
+    return 0;
 }
---
+++ Desafio_01p/prototipo/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//variavel Global...
+float luz =  299792458;
+
+int main () {
+
+    //declaração de variaveis ...
+    float distancia = 0;
+    float segundos = 0;
+    float minutos = 0; 
+    float horas = 0;
+
+
+
+    printf("=-=-=-=-=-=-=-=Olá Zorax seja Bem Vindo !!!=-=-=-=-=-=-=\n");
+    printf("Vamos calcular sua viagem ...\n");
+    printf("Vamos lá ...");
+    printf("-----------------------------------------------------------");
+
+    printf("Digite a distância entre dois planetas em quilômetros: ");
+    scanf("%f", distancia);
+
+    //
+
+    //processamento...
+    
+
+    
+
+
+
+
+}
