17eac68b88a6bfb05871f9692b470dac167ad920
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-07 20:45:53 -0300

    Update

--- Lista_02/exe_01/main.c
+++ Lista_02/exe_01/main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+/*01) Faça um código que receba um número inteiro (metros) e converta para polegadas.
+Dica: 1m = 100cm; 1 polegada = 2,54cm.
+-
+-
+Exemplo:
+5 metros = 196,85 polegadas.*/
+
+//Variais Globais.
+float POL = 39.37;
+
 int main () {
-    
+
+    //declaração de variaveis ...
+    float polegadas = 0;
+    int met = 0;
+
+    //Leitura de dados 
+    printf("Programa de conversão !!!\n");
+    printf("\n");
+
+    printf("Digite a quantidade de Metros: ");
+    scanf("%d", & met);
+
+    //conversão 
+    polegadas = met * POL;
+    printf("\n");
+
+    printf("%d metros equivalem a %.2f polegadas !", met, polegadas);
+    printf("\n");
+
+    return 0;
+
 }
