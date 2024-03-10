7a418d4fdb8475d9458b432488d5f1db30a5113d
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-10 20:59:26 -0300

    Atualizar o main.c

--- Lista_02/exe_09/main.c
+++ Lista_02/exe_09/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+/*
+
+09) Fazer um programa que receba um número inteiro, e verifique se ele está entre 100 e 200. Se estiver dentro desta faixa, imprima “Você digitou um número entre 100 e 200”, senão imprima “Você digitou um número fora da faixa”.
+
+*/
 int main () {
+
+// declaração de variaveis 
+int num = 0; 
+
+printf ("Digite um número inteiro: ");
+scanf("%d", &num);
+
+if ( num >=100 ) && ( num <= 200 ) {
+     printf ("Você Digitou um número entre 100            e 200");
     
 }
+else {
+     printf ("Você digitou um número fora da faixa".");
+}
+
+return 0;
