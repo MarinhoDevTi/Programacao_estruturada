f1d5bf17435d4a901ed8fb3502c086d755126c4b
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-10 17:35:29 -0300

    update

--- Lista_02/exe_07/main.c
+++ Lista_02/exe_07/main.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <math.h>
 
-int main () {
+/*07) Faça um programa para uma loja de tintas. 
+O programa deverá pedir o tamanho em metros 
+quadrados da área a ser pintada. Considere que a 
+cobertura da tinta é de 1 litro para cada 3 metros 
+quadrados e que a tinta é vendida em latas de 18 litros, 
+que custam R$ 80,00. Informe a quantidade de latas de tinta 
+a serem compradas e o preço total.*/
+
+#define COBERTURA_POR_LITRO 3
+#define TAMANHO_LATA 18
+#define PRECO_LATA 80.0
+
+int main() {
+    float tamanho_area, quantidade_litros, quantidade_latas, preco_total;
+
+    // Solicita ao usuário o tamanho da área a ser pintada em metros quadrados
+    printf("Digite o tamanho da área a ser pintada (em metros quadrados): ");
+    scanf("%f", &tamanho_area);
+
+    // Calcula a quantidade de litros necessários
+    quantidade_litros = (tamanho_area / COBERTURA_POR_LITRO);
+
+    // Calcula a quantidade de latas necessárias
+    quantidade_latas = (quantidade_litros / TAMANHO_LATA);
+
+    // Calcula o preço total
+    preco_total = quantidade_latas * PRECO_LATA;
+
+    // Exibe os resultados
+    printf("\n");
+    printf("Quantidade de latas de tinta necessárias: %.0f\n", quantidade_latas);
+    printf("Preço total: R$ %.2f\n", preco_total);
+
+    return 0;
     
 }
--- Lista_02/exe_08/main.c
+++ Lista_02/exe_08/main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
-int main () {
+/* 08) Faça um programa que peça o tamanho de 
+um arquivo para download (em MB) e a velocidade de 
+um link de Internet (em Mbps), calcule e informe o tempo
+ aproximado de download do arquivo usando este link (em minutos).
+*/
+
+float calcular_tempo_download(float tamanho_arquivo_mb, float velocidade_internet_mbps) {
+    // Convertendo a velocidade de Mbps para MBps
+    float velocidade_mbps_para_mbps = velocidade_internet_mbps / 8;
+    // Calculando o tempo de download em segundos
+    float tempo_download_segundos = tamanho_arquivo_mb / velocidade_mbps_para_mbps;
+    // Convertendo para minutos
+    float tempo_download_minutos = tempo_download_segundos / 60;
+    return tempo_download_minutos;
+}
+
+
+int main() {
+    float tamanho_arquivo_mb, velocidade_internet_mbps;
     
+    printf("Digite o tamanho do arquivo para download (em MB): ");
+    scanf("%f", &tamanho_arquivo_mb);
+
+    printf("Digite a velocidade do link de Internet (em Mbps): ");
+    scanf("%f", &velocidade_internet_mbps);
+
+    float tempo_download_minutos = calcular_tempo_download(tamanho_arquivo_mb, velocidade_internet_mbps);
+
+    printf("O tempo aproximado de download do arquivo é de: %.2f minutos.\n", tempo_download_minutos);
+
+    return 0;
 }
