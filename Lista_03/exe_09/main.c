8f556383453316e40bf4531789574e61613bf414
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-03-24 17:10:52 -0300

    update

--- Lista_03/exe_09/main.c
+++ Lista_03/exe_09/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+/*
+09) Faça um programa que calcule a soma de 
+todos os números de 1 a 10. Imprima todos os
+ números e a soma total.
+ 
+*/
+
 int main() {
+    int soma = 0;
+
+    // Loop para somar os números de 1 a 10 e imprimir cada número
+    printf("Números de 1 a 10:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", i);
+        soma += i;
+    }
+
+    // Imprime a soma total
+    printf("\nSoma total: %d\n", soma);
 
     return 0;
 }
-
--- Lista_03/exe_10/main.c
+++ Lista_03/exe_10/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+/*
+10) Faça um programa que calcule a soma 
+de todos os números pares entre 0 e 100. 
+Imprima os números e a soma total.
+*/
+
 int main() {
+    int soma = 0;
+
+    // Loop para somar os números pares de 0 a 100 e imprimir cada número
+    printf("Números pares entre 0 e 100:\n");
+    for (int i = 0; i <= 100; i += 2) {
+        printf("%d ", i);
+        soma += i;
+    }
+
+    // Imprime a soma total
+    printf("\nSoma total dos números pares: %d\n", soma);
 
     return 0;
 }
-
