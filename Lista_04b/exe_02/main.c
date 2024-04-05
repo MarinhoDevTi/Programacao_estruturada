350589ab851397945cc85fa98806be333c139174
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-04-04 21:53:35 -0300

    Update

--- Lista_04b/exe_02/main.c
+++ Lista_04b/exe_02/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-int main() {
+/*
 
+02) Escreva um programa em C para exibir o padrão como um triângulo de ângulo reto com um número. O usuário decide a altura do triângulo fornecendo um número inteiro. Exemplo: N=4.
+
+1
+12
+123
+1234
+
+*/
+
+int main() {
+    int altura;
+    
+    printf("Digite a altura do triângulo: ");
+    scanf("%d", &altura);
+    
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
 
--- Lista_04b/exe_03/main.c
+++ Lista_04b/exe_03/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main() {
+/*
 
+003) Escreva um programa em C para fazer um padrão como triângulo de ângulo reto com um número que repetirá um número em uma linha. O usuário decide a altura do triângulo fornecendo um número inteiro. Exemplo: N=4.
+
+1
+22
+333
+4444
+
+
+*/
+
+int main() {
+    int altura;
+    
+    printf("Digite a altura do triângulo: ");
+    scanf("%d", &altura);
+    
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
 
