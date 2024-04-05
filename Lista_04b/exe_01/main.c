631cef4c1bbb1b846ff3ca37f60fe4023fd467b8
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-04-04 21:37:05 -0300

    Update

--- Lista_04b/exe_01/main.c
+++ Lista_04b/exe_01/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-int main() {
+/*
 
+01) Escreva um programa em C para exibir o padrão como triângulo de ângulo reto usando um asterisco. O usuário decide a altura do triângulo fornecendo um número inteiro. Exemplo: N=4.
+
+*
+**
+***
+****
+
+
+*/
+
+int main() {
+    int altura;
+    
+    printf("Digite a altura do triângulo: ");
+    scanf("%d", &altura);
+    
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
-
