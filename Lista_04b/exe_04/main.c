ea81c8799c65310dae2348d132b4af4e7a9ecf59
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-04-04 21:57:47 -0300

    Update

--- Lista_04b/exe_04/main.c
+++ Lista_04b/exe_04/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main() {
+/*
 
+04) Escreva um programa em C para fazer um padrão como triângulo de ângulo reto com número aumentado de 1 em 1. O usuário decide a altura do triângulo fornecendo um número inteiro. Exemplo: N=4.
+
+1
+2 3
+4 5 6
+7 8 9 10
+
+*/
+
+int main() {
+    int altura;
+    int numero = 1;
+    
+    printf("Digite a altura do triângulo: ");
+    scanf("%d", &altura);
+    
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", numero);
+            numero++;
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
-
