ec3bb968a2564adfcb1cd67557b263ee872078f7
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-06-20 19:52:43 -0300

    Update

---
+++ Aulas_Material/aula_20062024/main.c
@@ -0,0 +1,9 @@
+// Para compilar codigos em C no VSCode
+//No terminal digite: gcc codigo.c -o codigo.exe
+//Para Rodar, digite no terminal: ./codigo.exe
+#include <stdio.h>
+
+int main(){
+    printf("Olá Mundo\n");
+    return 0;
+}
