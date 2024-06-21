ba291917cf27dc3878184bd73a87c4f6a7d9ba40
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-06-20 21:50:14 -0300

    Update

---
+++ Aulas_Material/aula_20062024/cinza.c
@@ -0,0 +1,40 @@
+//Uma imagem 100x100 fundo cinza claro, uma linha preta na diagonal. Escala de cinza.
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+    int i, j;
+    int matriz[100][100];
+
+    FILE *arquivo;
+
+    arquivo = fopen("dinairpgm.pgm", "w");
+
+    if(arquivo != NULL){
+        printf("Arquivo aberto com sucesso.\n");
+        for(i = 0; i < 100; i++){
+            for(j = 0; j < 100; j++){
+                if (i == j){
+                    matriz[i][j] = 0;
+                } else {
+                    matriz[i][j] = 225;
+                }
+            }
+        }
+        fprintf(arquivo, "P2\n");
+        fprintf(arquivo, "# Criado por Diogo\n");
+        fprintf(arquivo, "%d %d \n", 100, 100);
+        fprintf(arquivo, "%d \n", 255);
+        for(i = 0; i < 100; i++){
+            for(j = 0; j < 100; j++){
+                fprintf(arquivo, "%d ", matriz[i][j]);
+            }
+        }
+        fclose(arquivo);
+    } else {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ Aulas_Material/aula_20062024/pb.c
@@ -0,0 +1,43 @@
+//Uma imagem P&B, 200x100, metade da direita preta, metade da esquerda branca.
+
+#include<stdio.h>
+
+int main(){
+    int i, j;
+    int matriz[100][200];
+    FILE *arquivo;
+    arquivo = fopen("dinairpbm.pbm", "w");
+
+    if(arquivo != NULL){
+        printf("Arquivo aberto com sucesso.\n");
+        for(i=0;i<100;i++){
+            for(j=0;j<200;j++){
+                if((i==0) || (i==99)){
+                    matriz[i][j] = 1;
+                } else {
+                    if(j<100){
+                        matriz[i][j] = 1;
+                    } else if(j==199) {
+                        matriz[i][j] = 1;
+                    } else {
+                        matriz[i][j] = 0;
+                    }
+                }
+            }
+        }
+        fprintf(arquivo, "P1\n");
+        fprintf(arquivo, "# Criado por André\n");
+        fprintf(arquivo, "%d %d \n", 200, 100);
+        for(i=0;i<100;i++){
+            for(j=0;j<200;j++){
+                fprintf(arquivo, "%d ", matriz[i][j]);
+            }
+        }
+        fclose(arquivo);
+    } else {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    return 0;
+}
