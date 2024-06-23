64c6c745df22522ed0e29e14f5baa28757650e1c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-06-23 17:25:55 -0300

    Update

--- Listas/Lista_09/exe_01/main.c
+++ Listas/Lista_09/exe_01/main.c
@@ -1,7 +1,46 @@
+
 #include <stdio.h>
 
-int main() {
+int main(){
+    srand(time(NULL));
+    int i, j, k, n;
+    int matriz[100][100];
 
-    return 0;
-}
+    FILE *arquivo;
 
+    arquivo = fopen("imagem.pbm", "w");
+
+    if(arquivo != NULL){
+        printf("Arquivo criado com sucesso\n");
+        for (i = 0; i < 100; i++){
+            for(j = 0; j < 100; j++){
+                //for( k = 0; k < 3; k ++){
+                    n = rand() % 2;
+                    matriz [i][j] = n;
+                }
+            }
+        }
+
+        fprintf(arquivo, "P1\n");
+        fprintf(arquivo, "#imagem.pbm \n");
+        fprintf(arquivo, "%d %d\n", 100, 100);
+        //fprintf(arquivo, "%d\n", 255);
+        for (i = 0; i < 100; i++){
+            for(j = 0; j < 100; j++){
+                for( k = 0; k < 3; k++ ){
+                    fprintf(arquivo, "%d ", matriz[i][j]);
+                }
+            }
+        }
+        fclose(arquivo);
+
+    } else {
+        printf("Erro ao criar o arquivo.\n ");
+        return 0;
+    }
+
+
+
+
+    
+    
