d8fbf573b6425ee634f00bfab439abb71147ef5e
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-08-08 23:45:25 -0300

    Update

--- Desafios/Desafio_12/main.c
+++ Desafios/Desafio_12/main.c
@@ -1,48 +1,63 @@
-//Para compilar códigos em C no VSCode
-//No terminal digite: gcc codigo.c -o codigo.exe
-//Para rodar, digite no terminal: ./codigo.exe
-
 #include<stdio.h>
 #include<time.h>
 
 int main(){
-    srand(time(NULL));
-    int i, j, k, n;
-    int matriz[100][100][3];
+    int largura, altura, valor_maximo;
+    //int i, j, k;
+    char buffer[255];
 
     FILE *arquivo;
+    FILE *saida;
 
-    arquivo = fopen("imagem.ppm", "w");
+    arquivo = fopen("imagem.ppm", "r");
+    saida   = fopen("imagem.ppm", "w");
 
-    if(arquivo != NULL){
-        printf("Arquivo criado com sucesso!\n");
-        for(i = 0; i < 100; i++){
-            for(j = 0; j < 100; j++){
-                for(k = 0; k < 3; k++){
-                    n = rand() % 256;
-                    matriz[i][j][k] = n;
+    if(arquivo == NULL){
+        printf("Erro na abertura do arquivo.\n");
+        return 1;
+    } else {
+        printf("Arquivo aberto com sucesso.\n");
+        //Leitura do arquivo de entrada
+        fgets(buffer, 255, arquivo);//Lê a primeira linha do arquivo
+        printf("%s", buffer);
+        fgets(buffer, 255, arquivo);//Lê a segunda linha do arquivo
+        printf("%s", buffer);
+        fscanf(arquivo, "%d %d", &largura, &altura);//Lê a terceira linha do arquivo
+        printf("Largura: %d\nAltura: %d\n", largura, altura);
+        int matriz[largura][altura][3];
+        fscanf(arquivo, "%d", &valor_maximo);//Lê a quarta linha do arquivo
+        for(int i=0;i<largura;i++){
+            for(int j=0;j<altura;j++){
+                for(int k=0;k<3;k++){
+                    fscanf(arquivo, "%d", &matriz[i][j][k]);                    
                 }
             }
         }
+        printf("%d %d %d", matriz[0][0][0], matriz[0][0][1], matriz[0][0][2]);
 
-        fprintf(arquivo, "P3\n");
-        fprintf(arquivo, "\n");
-        fprintf(arquivo, "%d %d\n", 100, 100);
-        fprintf(arquivo, "%d\n", 255);
-        for(i = 0; i < 100; i++){
-            for(j = 0; j < 100; j++){
-                for(k = 0; k < 3; k++){
-                    fprintf(arquivo, "%d ", matriz[i][j][k]);
+        //Escrever a arara cinza em formato PPM P3
+        int pixel;
+        int soma = 0;
+        fprintf(saida, "P3\n");
+        fprintf(saida, "\n");
+        fprintf(saida, "%d %d\n", largura, altura);
+        fprintf(saida, "255\n");
+        for(int i=0;i<largura;i++){
+            for(int j=0;j<altura;j++){
+                for(int k=0;k<3;k++){
+                    soma += matriz[i][j][k];
+                    //fprintf(saida, "%d ", 255-matriz[i][j][k]); //Efeito negativo
                 }
+                pixel = soma/3;
+                fprintf(saida, "%d %d %d ", pixel, pixel, pixel);
+                soma = 0;
             }
         }
+
         fclose(arquivo);
+        fclose(saida);
 
-    }else{
-        printf("Erro ao criar o arquivo.\n");
-        return 0;
     }
 
-    return 1;
-
+    return 0;
 }
