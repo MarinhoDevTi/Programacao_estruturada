8347a6734acbc3b994c08731c6cc1247fc92ddde
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-08-08 21:43:16 -0300

    Update main.c

--- Desafios/Desafio_04/exe_02/main.c
+++ Desafios/Desafio_04/exe_02/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    int soma = 0;
+
+    // Loop para percorrer todos os números abaixo de 1000
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            soma += i;  // Soma os múltiplos de 3 ou 5
+        }
+    }
+
+    printf("A soma de todos os múltiplos de 3 ou 5 abaixo de 1000 é: %d\n", soma);
 
     return 0;
 }
-
