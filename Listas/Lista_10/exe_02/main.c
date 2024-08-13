580c3c358f7572eaeb0ccda88abbd71d423255b8
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2024-08-12 23:38:16 -0300

    Update

---
+++ Listas/Lista_10/exe_02/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+typedef struct {
+    unsigned char r, g, b;
+} Pixel;
+
+void mirrorHorizontal(Pixel **image, int width, int height) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width / 2; j++) {
+            Pixel temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
+int main() {
+    FILE *inputFile = fopen("imagem.ppm", "rb");
+    FILE *outputFile = fopen("horizontal_mirror_imagem.ppm", "wb");
+
+    if (inputFile == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return 1;
+    }
+
+    char format[3];
+    int width, height, maxval;
+    fscanf(inputFile, "%s", format);
+    fscanf(inputFile, "%d %d", &width, &height);
+    fscanf(inputFile, "%d", &maxval);
+
+    Pixel **image = (Pixel **)malloc(height * sizeof(Pixel *));
+    for (int i = 0; i < height; i++) {
+        image[i] = (Pixel *)malloc(width * sizeof(Pixel));
+        fread(image[i], sizeof(Pixel), width, inputFile);
+    }
+
+    mirrorHorizontal(image, width, height);
+
+    fprintf(outputFile, "%s\n%d %d\n%d\n", format, width, height, maxval);
+    for (int i = 0; i < height; i++) {
+        fwrite(image[i], sizeof(Pixel), width, outputFile);
+    }
+
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return 0;
+}
